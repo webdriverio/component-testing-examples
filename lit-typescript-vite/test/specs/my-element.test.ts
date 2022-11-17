e6c3f182f5e6153a6ef3c7a84eb3b0a32e6a0693
Christian Bromann <git@bromann.dev>
2022-11-17 00:45:38 -0800

    improve way to render Lit component

--- lit-typescript-vite/test/specs/my-element.test.ts
+++ lit-typescript-vite/test/specs/my-element.test.ts
@@ -1,17 +1,16 @@
+import { html, render } from 'lit'
 import { $, expect } from '@wdio/globals'
 
 import '../../src/my-element'
 
 describe('Lit component testing', () => {
     it('should increment value on click', async () => {
-        const elem = document.createElement('my-element')
-        document.body.appendChild(elem)
+        render(
+            html`<my-element />`,
+            document.body
+        )
 
-        /**
-         * Deep selector is not yet supported in browser land due to its CJS
-         * dependencies, see https://github.com/webdriverio/webdriverio/issues/9126
-         */
-        const button = (await $(elem)).$('>>>button')
+        const button = (await $('my-element')).$('>>>button')
         await expect(button).toHaveText('count is 0')
 
         await button.click()
