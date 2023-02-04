2658b2f3d8b0eb2e3f5f0560164e5e43169df01a
Christian Bromann <git@bromann.dev>
2023-02-03 21:10:24 -0500

    use latest mock capabilities

--- react-typescript-vite/src/tests/LoginForm.test.tsx
+++ react-typescript-vite/src/tests/LoginForm.test.tsx
@@ -1,85 +1,89 @@
 /// <reference types="@wdio/globals/types" />
 import React from 'react'
 import { $, expect } from '@wdio/globals'
+import { fn } from '@wdio/browser-runner'
 import { Key } from 'webdriverio'
 import { render } from '@testing-library/react'
 import LoginForm from '../components/LoginForm'
 
 describe('LoginForm', () => {
-  const onLogin = (...args: any[]) => console.log('YES!', ...args)
+  const onLogin = fn()
 
   it('should have password input of type password', async () => {
-    render(<LoginForm onLogin={() => {}} />);
+    render(<LoginForm onLogin={onLogin} />)
     await expect($('input[type="password"]')).toBePresent()
-  });
+  })
 
   it('should render title with default text', async () => {
-    render(<LoginForm onLogin={() => {}} />);
+    render(<LoginForm onLogin={onLogin} />)
     await expect($('legend=Log In')).toBePresent()
-  });
+  })
 
   it('should render title with specified text', async () => {
-    const title = 'Please Authenticate';
-    render(<LoginForm title={title} onLogin={() => {}} />);
+    const title = 'Please Authenticate'
+    render(<LoginForm title={title} onLogin={onLogin} />)
     await expect($(`legend=${title}`)).toBePresent()
-  });
+  })
 
   describe('form tests', () => {
-    const username = 'testuser123';
-    const password = 's3cret';
+    const username = 'testuser123'
+    const password = 's3cret'
     let $username: WebdriverIO.Element
     let $password: WebdriverIO.Element
     let $btnLogin: WebdriverIO.Element
     let $form: WebdriverIO.Element
 
     beforeEach(async () => {
-      const { container } = render(<LoginForm onLogin={onLogin} />);
-      $username = await $('input[name="username"]');
-      $password = await $('input[name="password"]');
+      const { container } = render(<LoginForm onLogin={onLogin} />)
+      $username = await $('input[name="username"]')
+      $password = await $('input[name="password"]')
       $btnLogin = await $('button=Login')
       $form = await $(container)
-    });
+      onLogin.mockClear()
+    })
 
     it('should call onLogin with username and password when the Login button is clicked', async () => {
-      await $username.setValue(username);
-      await $password.setValue(password);
-      await $btnLogin.click();
-      // ToDo(Christian): verify login function got called
-    });
+      await $username.setValue(username)
+      await $password.setValue(password)
+      await $btnLogin.click()
+      expect(onLogin).toBeCalledTimes(1)
+      expect(onLogin).toBeCalledWith({ username, password })
+    })
 
     it('should call onLogin with username and password when enter is pressed in an input', async () => {
-      await $username.setValue(username);
-      await $password.setValue(password);
+      await $username.setValue(username)
+      await $password.setValue(password)
       await browser.keys(Key.Enter)
-      // ToDo(Christian): verify login function got called
-    });
+      expect(onLogin).toBeCalledTimes(1)
+      expect(onLogin).toBeCalledWith({ username, password })
+    })
 
     it('should show both validation errors if login is attempted without entering username or password', async () => {
-      await $btnLogin.click();
+      await $btnLogin.click()
       await expect($form).toHaveTextContaining('Username is required')
       await expect($form).toHaveTextContaining('Password is required')
-      // ToDo(Christian): verify login function was not called
-    });
+      expect(onLogin).toBeCalledTimes(0)
+    })
 
     it('should only show password validation error if login is attempted without entering password', async () => {
-      await $username.setValue(username);
-      await $btnLogin.click();
+      await $username.setValue(username)
+      await $btnLogin.click()
       await expect($form).not.toHaveTextContaining('Username is required')
       await expect($form).toHaveTextContaining('Password is required')
-      // ToDo(Christian): verify login function was not called
-    });
+      expect(onLogin).toBeCalledTimes(0)
+    })
 
     it('should only show username validation error if login is attempted without entering username', async () => {
-      await $password.setValue(username);
-      await $btnLogin.click();
+      await $password.setValue(username)
+      await $btnLogin.click()
       await expect($form).toHaveTextContaining('Username is required')
       await expect($form).not.toHaveTextContaining('Password is required')
-      // ToDo(Christian): verify login function was not called
-    });
+      expect(onLogin).toBeCalledTimes(0)
+    })
 
     it('should not show any validation errors before login is attempted', async () => {
       await expect($form).not.toHaveTextContaining('Username is required')
       await expect($form).not.toHaveTextContaining('Password is required')
-    });
-  });
+    })
+  })
 })
