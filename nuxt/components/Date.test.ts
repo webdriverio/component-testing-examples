285262ceba0d8db702c5ac340b504d767e377a05
Christian Bromann <git@bromann.dev>
2023-08-06 15:22:24 -0700

    fix date test

--- nuxt/components/Date.test.ts
+++ nuxt/components/Date.test.ts
@@ -4,10 +4,14 @@ import { render } from '@testing-library/vue'
 import DateComponent from './Date.vue'
 
 describe('Date Component', () => {
-    it('should display date correctly', async () => {
-        render(DateComponent, { props: { date: '2043' } })
+    it.only('should display date correctly', async () => {
+        const date = new Date('2043')
+        const localTime = date.toLocaleString("en-US", {
+          timeZone: "America/New_York"
+        })
+        render(DateComponent, { props: { date: localTime } })
         const component = await $('span')
         await expect(component).toBePresent()
-        await expect(component).toHaveText('31/12/2042, 16:00:00')
+        await expect(component).toHaveText('31/12/2042, 19:00:00')
     })
 })
